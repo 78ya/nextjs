2c83647d590bf011ede7a932b08f788790a7a351
朝歌夜弦 <bop-lp@qq.com>
2025-12-09 12:50:00 +0800

    refactor: 移除nextConfig中的experimental配置

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    // 启用 instrumentation hook，允许在服务器启动时运行初始化代码
-    instrumentationHook: true,
-  },
 };
 
 export default nextConfig;
